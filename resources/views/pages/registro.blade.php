f8a6bea24856521d1343edad65e185fcd121f34f
javierpaiz <hpaizramos@gmail.com>
2024-11-21 22:04:45 -0600

    cambio css

--- resources/views/pages/registro.blade.php
+++ resources/views/pages/registro.blade.php
@@ -4,6 +4,8 @@
 <link href="{{asset('css/style.css')}}" rel="stylesheet" />
 <link href="../css/style.css" rel="stylesheet" />
 <link href="../css/responsive.css" rel="stylesheet" />
+<link href="{{asset('css/responsive.css')}}" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
 @livewire('page.registro-actividad', ['idActividad'=>$idActividad])
 
 <x-footer/>
