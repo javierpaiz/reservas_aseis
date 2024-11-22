ff779ebffee7bea1723af63d559a0bae1c35b2ef
javierpaiz <hpaizramos@gmail.com>
2024-11-21 21:55:46 -0600

    change en rutas de css

--- resources/views/pages/registro.blade.php
+++ resources/views/pages/registro.blade.php
@@ -1,4 +1,8 @@
 <x-header/>
+<link rel="stylesheet" href="../css/estilonoticias.css">
+<link rel="stylesheet" href="{{asset('css/estilonoticias.css')}}">
+<link href="{{asset('css/style.css')}}" rel="stylesheet" />
+<link href="../css/style.css" rel="stylesheet" />
 @livewire('page.registro-actividad', ['idActividad'=>$idActividad])
 
 <x-footer/>
