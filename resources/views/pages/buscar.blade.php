ce3cbb9aa9cd5ed4b0c2dd4af4ab32ca2d9cd1fc
javierpaiz <hpaizramos@gmail.com>
2024-11-21 21:58:59 -0600

    change en rutas de css

--- resources/views/pages/buscar.blade.php
+++ resources/views/pages/buscar.blade.php
@@ -1,3 +1,5 @@
 <x-header />
+<link rel="stylesheet" href="../css/estilonoticias.css">
+<link rel="stylesheet" href="{{asset('css/estilonoticias.css')}}">
 @livewire('page.buscar-registro')
 <x-footer />
--- resources/views/pages/registro.blade.php
+++ resources/views/pages/registro.blade.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" href="{{asset('css/estilonoticias.css')}}">
 <link href="{{asset('css/style.css')}}" rel="stylesheet" />
 <link href="../css/style.css" rel="stylesheet" />
+<link href="../css/responsive.css" rel="stylesheet" />
 @livewire('page.registro-actividad', ['idActividad'=>$idActividad])
 
 <x-footer/>
