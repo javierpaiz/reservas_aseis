33c6680d64c745849886109d5cdbad4dcb25ff28
javierpaiz <hpaizramos@gmail.com>
2024-11-25 20:33:07 -0600

    Feat:Lista de actividades e informe de inscritos

--- resources/views/pages/informe-inscritos.blade.php
+++ resources/views/pages/informe-inscritos.blade.php
@@ -1,6 +1,13 @@
 <x-header />
 <link rel="stylesheet" href="../css/estilonoticias.css">
 <link rel="stylesheet" href="{{asset('css/estilonoticias.css')}}">
+<link rel="stylesheet" href="../css/estilonoticias.css">
+<link rel="stylesheet" href="{{asset('css/estilonoticias.css')}}">
+<link href="{{asset('css/style.css')}}" rel="stylesheet" />
+<link href="../css/style.css" rel="stylesheet" />
+<link href="../css/responsive.css" rel="stylesheet" />
+<link href="{{asset('css/responsive.css')}}" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
 
 <section class="contact_section layout_padding">
     <div class="container contact_heading">
